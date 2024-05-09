0e258f39bb42fa723a285c8739622d44ffcdc43d
leoste <35530198+leoste@users.noreply.github.com>
2024-05-09 13:36:47 +0300

    Update flac_to_opus.ps1

--- windows/flac_to_opus.ps1
+++ windows/flac_to_opus.ps1
@@ -1,7 +1,12 @@
 $parent = (Get-Location).Path + " (opus)";
-Get-ChildItem -Recurse *.flac | foreach-object {    
-    $child = ($_ | Resolve-Path -Relative).subString(1);
-    $name = $parent + $child.Remove($child.Length - $_.Extension.Length) + ".opus";        
-    New-Item -ItemType File -Path $name -Force;
-    opusenc --bitrate 160 "$_" "$name" --quiet;    
+Get-ChildItem -Recurse *.flac | foreach-object {	
+	$child = ($_ | Resolve-Path -Relative).subString(1);
+	$name = $parent + $child.Remove($child.Length - $_.Extension.Length) + ".opus";		
+	if (Test-Path -PathType Leaf -Path $name) {
+		Write-Host "ERROR: [$name] already exists.";
+	}
+	else {
+		New-Item -ItemType File -Path $name -Force;
+		opusenc --bitrate 256 "$_" "$name" --quiet;
+	}	
 }
